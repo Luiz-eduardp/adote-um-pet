e49d62239a7cc6d4917d6585254bf1825123e4c1
Luiz Eduardo <luizzeduardp@gmail.com>
2022-01-12 09:29:28 -0300

    Update contato.vue

--- src/pages/contato.vue
+++ src/pages/contato.vue
@@ -5,17 +5,6 @@
       Envie os dados do bichinho que quer doar por
         <a href="mailto:luizzeduardp@gmail.com">email</a>
 
-        <form name="contact" netlify>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
       </q-card-section>
     </q-card>
   </q-page>
